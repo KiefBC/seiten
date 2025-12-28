c97d72c8f71d89acce9dcc040f1dd5d1963f8b77
KiefBC <kiefhay642@outlook.com>
2025-12-28 11:41:08 -0800

    initial tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.rs",
+    "./server/**/*.rs",
+    "./frontend/**/*.rs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
